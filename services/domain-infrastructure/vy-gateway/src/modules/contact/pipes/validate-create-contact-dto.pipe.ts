e831562fadc18a2d425f732e9aa0ea9b65ee75b8
cperillatesting <cperilla@sabanatech.com>
2025-07-02 14:37:47 -0300

    fix gateway contact handling

--- services/domain-infrastructure/vy-gateway/src/modules/contact/pipes/validate-create-contact-dto.pipe.ts
+++ services/domain-infrastructure/vy-gateway/src/modules/contact/pipes/validate-create-contact-dto.pipe.ts
@@ -12,7 +12,7 @@ export class ValidateCreateContactDtoPipe implements PipeTransform {
     value: CreateContactDto,
     metadata: ArgumentMetadata,
   ): CreateContactDto {
-    if (!value.firstName && !value.lastName && !value.email && !value.formId) {
+    if (!value.firstName || !value.lastName || !value.email || !value.formId) {
       throw new BadRequestException(
         `You must provide all require fields. Got metadata=>${metadata}`,
       );
