925b988f0f14e30a4b060190c3909285b1092657
cperillatesting <cperilla@sabanatech.com>
2025-07-03 20:45:28 -0300

    fix: resolve compilation errors

--- services/domain-person-and-identity/vy-person-identity/src/modules/person/person.module.ts
+++ services/domain-person-and-identity/vy-person-identity/src/modules/person/person.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Person } from "../../entities/person.entity";
+import { Email } from "../../entities/email.entity";
+import { BusinessUnit } from "../../../../domain-multitenant/ez-business-operators/src/entities/business-unit.entity";
 import { PersonService } from "./services/person.service";
 import { PersonKafkaService } from "./services/person-kafka.service";
 import { PersonController } from "./person.controller";
@@ -8,7 +10,7 @@ import { getLoggerConfig } from "../../utils/common";
 import { LogStreamLevel } from "ez-logger";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Person])],
+  imports: [TypeOrmModule.forFeature([Person, Email, BusinessUnit])],
   controllers: [PersonController],
   providers: [PersonService, PersonKafkaService],
 })
