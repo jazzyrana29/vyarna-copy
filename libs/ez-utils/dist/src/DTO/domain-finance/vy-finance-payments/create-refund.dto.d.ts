a362b595c96cfedc247fcb0be0072f25b8657062
Carlos Perilla <carlosperillaprogramacion@gmail.com>
2025-07-07 18:53:20 -0300

    .

--- libs/ez-utils/dist/src/DTO/domain-finance/vy-finance-payments/create-refund.dto.d.ts
+++ libs/ez-utils/dist/src/DTO/domain-finance/vy-finance-payments/create-refund.dto.d.ts
@@ -1,4 +1,5 @@
-declare const CreateRefundDto_base: any;
+import { RefundDto } from './refund.dto';
+declare const CreateRefundDto_base: import("@nestjs/common").Type<Pick<RefundDto, "paymentIntentId" | "amountCents" | "reason" | "metadata">>;
 export declare class CreateRefundDto extends CreateRefundDto_base {
 }
 export {};
--- libs/ez-utils/dist/src/constants/kafka-topics/domain-finance/vy-finance-payments/payment-intent.js
+++ libs/ez-utils/dist/src/constants/kafka-topics/domain-finance/vy-finance-payments/payment-intent.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.KT_GET_ZTRACKING_PAYMENT_INTENT = exports.KT_GET_PAYMENT_INTENT = exports.KT_CREATE_PAYMENT_INTENT = void 0;
+exports.KT_CREATE_REFUND = exports.KT_GET_ZTRACKING_PAYMENT_INTENT = exports.KT_GET_PAYMENT_INTENT = exports.KT_CREATE_PAYMENT_INTENT = void 0;
 exports.KT_CREATE_PAYMENT_INTENT = 'create-payment-intent';
 exports.KT_GET_PAYMENT_INTENT = 'get-payment-intent';
 exports.KT_GET_ZTRACKING_PAYMENT_INTENT = 'get-ztracking-payment-intent';
+exports.KT_CREATE_REFUND = 'create-refund';
 //# sourceMappingURL=payment-intent.js.map
