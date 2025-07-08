1cbd3bfda75d990380399213433453567595ba95
Carlos Perilla <carlosperillaprogramacion@gmail.com>
2025-07-08 00:51:10 -0300

    DIST

--- libs/ez-utils/dist/src/DTO/domain-health-and-insights/vy-health-sleep/get-ztracking-sleep-schedules.dto.d.ts
+++ libs/ez-utils/dist/src/DTO/domain-health-and-insights/vy-health-sleep/get-ztracking-sleep-schedules.dto.d.ts
@@ -1,4 +1,5 @@
 import { SleepScheduleDto } from './sleep-schedule.dto';
-declare const GetZtrackingSleepSchedulesDto_base: import("@nestjs/common").Type<Pick<SleepScheduleDto, 'babyId'>>;
-export declare class GetZtrackingSleepSchedulesDto extends GetZtrackingSleepSchedulesDto_base {}
-export {}
+declare const GetZtrackingSleepSchedulesDto_base: import("@nestjs/common").Type<Pick<SleepScheduleDto, "babyId">>;
+export declare class GetZtrackingSleepSchedulesDto extends GetZtrackingSleepSchedulesDto_base {
+}
+export {};
