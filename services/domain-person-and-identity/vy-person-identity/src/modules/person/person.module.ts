18a35bd2d0150753679093991039b95b1393145f
cperillatesting <cperilla@sabanatech.com>
2025-07-08 00:37:50 -0300

    fix: include ztracking entity in person module

--- services/domain-person-and-identity/vy-person-identity/src/modules/person/person.module.ts
+++ services/domain-person-and-identity/vy-person-identity/src/modules/person/person.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Person } from "../../entities/person.entity";
 import { Email } from "../../entities/email.entity";
 import { BusinessUnit } from "../../entities/business-unit.entity";
+import { ZtrackingPerson } from "../../entities/ztracking-person.entity";
 import { PersonService } from "./services/person.service";
 import { PersonKafkaService } from "./services/person-kafka.service";
 import { ZtrackingPersonService } from "./services/ztracking-person.service";
@@ -12,7 +13,7 @@ import { getLoggerConfig } from "../../utils/common";
 import { LogStreamLevel } from "ez-logger";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Person, Email, BusinessUnit])],
+  imports: [TypeOrmModule.forFeature([Person, Email, BusinessUnit, ZtrackingPerson])],
   controllers: [PersonController],
   providers: [
     PersonService,
