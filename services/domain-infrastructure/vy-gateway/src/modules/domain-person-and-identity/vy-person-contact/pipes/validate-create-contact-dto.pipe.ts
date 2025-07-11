66a25fc33aec6b20e33248204b84c12e032386ad
cperillatesting <cperilla@sabanatech.com>
2025-07-11 01:08:34 -0300

    feat(gateway): add person contact module

---
+++ services/domain-infrastructure/vy-gateway/src/modules/domain-person-and-identity/vy-person-contact/pipes/validate-create-contact-dto.pipe.ts
@@ -0,0 +1,18 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { CreateContactDto } from 'ez-utils';
+
+@Injectable()
+export class ValidateCreateContactDtoPipe implements PipeTransform {
+  transform(value: CreateContactDto, _metadata: ArgumentMetadata) {
+    const { firstName, lastName, email } = value as any;
+    if (!firstName || !lastName || !email) {
+      throw new BadRequestException('Missing required fields');
+    }
+    return value;
+  }
+}
