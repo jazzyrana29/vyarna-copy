3382f2bfc50a8f4e8901939e993d503bf0ec626d
jazzy <jazib@antsoft.com>
2025-07-15 09:28:41 +0500

    fix zustand persist error import.meta

--- apps/web-app/babel.config.js
+++ apps/web-app/babel.config.js
@@ -2,15 +2,18 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+      [
+        'babel-preset-expo',
+        { jsxImportSource: 'nativewind', unstable_transformImportMeta: true },
+      ],
+      'nativewind/babel',
     ],
     plugins: [
       [
-        "module:react-native-dotenv",
+        'module:react-native-dotenv',
         {
-          moduleName: "@env",
-          path: ".env",
+          moduleName: '@env',
+          path: '.env',
           allowUndefined: true,
         },
       ],
