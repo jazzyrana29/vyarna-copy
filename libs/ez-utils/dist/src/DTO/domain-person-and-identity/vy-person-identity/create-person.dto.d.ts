961ffec0330cf9c38c59ab2f0a9265131916e0f3
jazzy <jazib@antsoft.com>
2025-07-15 11:58:41 +0500

    build ez-utils

--- libs/ez-utils/dist/src/DTO/domain-person-and-identity/vy-person-identity/create-person.dto.d.ts
+++ libs/ez-utils/dist/src/DTO/domain-person-and-identity/vy-person-identity/create-person.dto.d.ts
@@ -1,6 +1,6 @@
 import { PersonDto } from "./person.dto";
 import { TagsEnum } from "../../../enums/domain-person-and-identity/vy-active-campaign/tags.enum";
-declare const CreatePersonDto_base: import("@nestjs/common").Type<Pick<PersonDto, "rootBusinessUnitId" | "username" | "nameFirst" | "nameMiddle" | "nameLastFirst" | "nameLastSecond" | "password" | "roles" | "updatedBy">>;
+declare const CreatePersonDto_base: import("@nestjs/common").Type<Pick<PersonDto, "rootBusinessUnitId" | "roles" | "username" | "nameFirst" | "nameMiddle" | "nameLastFirst" | "nameLastSecond" | "password" | "updatedBy">>;
 export declare class CreatePersonDto extends CreatePersonDto_base {
     email: string;
     addInActiveCampaign?: boolean;
