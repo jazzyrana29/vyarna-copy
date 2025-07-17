7da7d83da1c01b3f9d47bc346240f9468bb883cb
jazzyrana29 <jazib.rana@ezwaveflow.com>
2025-07-17 16:10:05 +0500

    Fix quantity update to only use add socket

--- apps/web-app/src/components/Cart.tsx
+++ apps/web-app/src/components/Cart.tsx
@@ -78,15 +78,16 @@ const Cart: FC<CartProps> = ({ visible, onClose, onBackToProducts }) => {
     updateQuantity(productId, newQuantity);
     if (!cartId) return;
     try {
-      await socketRemoveCartItem(
-        'sales-commerce',
-        { cartId, productId },
-        { skipLoading: true },
-      );
       if (newQuantity > 0) {
         await socketAddCartItem(
           'sales-commerce',
           { cartId, productId, quantity: newQuantity },
+          { skipLoading: true },
+        );
+      } else {
+        await socketRemoveCartItem(
+          'sales-commerce',
+          { cartId, productId },
           { skipLoading: true },
         );
       }
